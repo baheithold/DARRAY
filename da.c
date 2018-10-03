7ac7d189335e38d530c2955c4a17cb5634176376
baheithold <baheithold@crimson.ua.edu>
2018-10-02 22:37:53 -0500

    Implemented newDA and setDAdisplay methods.

---
+++ da.c
@@ -0,0 +1,28 @@
+#include "da.h"
+#include <assert.h>
+#include <stdlib.h>
+
+#define FACTOR 2
+
+struct DA {
+    int capacity;
+    int size;
+    void **store;
+
+    void (*display)(void *, FILE *);
+    void (*free)(void *);
+};
+
+DA *newDA(void) {
+    DA *da = malloc(sizeof(DA));
+    assert(da != 0);
+    da->capacity = 1;
+    da->size = 0;
+    da->store = malloc(sizeof(void**));
+    return da;
+}
+
+void setDAdisplay(DA *items, void (*display)(void *, FILE *)) {
+    assert(items != 0);
+    items->display = display;
+}
