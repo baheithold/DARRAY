7c9a8148d1ccd51700a8cf9ec4697afb6aff95f1
baheithold <baheithold@crimson.ua.edu>
2018-10-02 21:53:09 -0500

    Started working on header file.

---
+++ da.h
@@ -0,0 +1,24 @@
+#ifndef __DA_H__
+#define __DA_H__
+
+#include <stdio.h>
+
+typedef struct da DA;
+
+extern DA *newDA(void);
+extern void setDAdisplay(DA *items, void (*display)(void *, FILE *));
+extern void setDAfree(DA *items, void (*free)(void *));
+extern void insertDA(DA *items, int index, void *value);
+extern void *removeDA(DA *items, int index);
+extern void unionDA(DA *recipient, DA *donor);
+extern void *getDA(DA *items, int index);
+extern void *setDA(DA *items, int index, void *value);
+extern int sizeDA(DA *items);
+extern void displayDA(DA *items, FILE *fp);
+extern int debugDA(DA *items, int index);
+extern void freeDA(DA *items);
+
+#define insertDAback(items, value)  insertDA(items, sizeDA(items), value)
+#define removeDAback(items)         removeDA(items, sizeDA(items) - 1)
+
+#endif // !__DA_H__
